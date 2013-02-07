699b4d4b10b996fbfdadb2271921a23045460aa6
Buckley Robinson <brobinson@verticalresponse.com>
2013-02-07 07:12:44 -0800

    removed old files from a repo far, far away

--- chrome/bootstrap.js
+++
@@ -1,6 +0,0 @@
-var head = document.getElementsByTagName('head')[0],
-  script = document.createElement('script');
-
-script.src = 'http://fatwallet.dev/javascripts/main.js';
-
-//head.appendChild( script );
